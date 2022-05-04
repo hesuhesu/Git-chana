b5b14e336df6e642e7b8340d7a61c170cdc59764
hesuhesu <91324571+hesuhesu@users.noreply.github.com>
2022-05-04 09:45:29 +0900

    Create practice.py

---
+++ practice1/practice.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name = 'practice',
+  version = '1.0.0',
+  description = 'Git-chana',
+  author = 'hesuhesu'
+  url = "https://github.com/hesuhesu/Git-chana",
+  install_requires=[''],
+  long_description = 'Git-chana practice python module',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
