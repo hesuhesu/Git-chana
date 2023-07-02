5032dcc7ecc8dcce356b8c5fb232f5e5a48d3a6a
hesuhesu <zndwk0728@gmail.com>
2023-07-02 21:12:58 +0900

    upload day7

---
+++ android/day7/MyIntent/app/src/main/java/org/techtown/intent/MenuActivity.java
@@ -0,0 +1,30 @@
+package org.techtown.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+
+                setResult(200, intent);
+                finish();
+            }
+        });
+    }
+}
